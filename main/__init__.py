6aaeaeb28165222859037d5ce062e2388eb0865b
davidshroff27 <125856152+davidshroff27@users.noreply.github.com>
2023-02-20 12:30:38 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("25906376", default=None, cast=int)
+API_HASH = config("27573285f886c6b3a647f5656d56bbb1", default=None)
+BOT_TOKEN = config("6132685416:AAFLbmlOO1x_ZWwSSok4U_5XzmTprhapu8U", default=None)
+SESSION = config("AgA55ZOninwap271VdKbdpzLass8PFwa0ta5Pb75PI8zgc0ghC-ZhjGwmK4eTwmE1rxcputyphjuRIGC0qd55lvPZ0fZ9hzVP9SqD08_SUEXOQ8U_MM95AGUXBrRksBFdzt1fj8WF6mdCJogpKBjLfaNLjvQeWmHI73TqSwdaanw-QbB_6kL17HWlS0qhPWWPtLViU4aVLyUO8zrCmFXhwWkmY1fkTrhnpIV4l5n3I9BaDFleEIsA6HN7TaZ2eMdXMU5Prt2kuJNFivXl_e261xrSoX0dPCvuLpbdvrBQQWOs-fn3MLn0wJjwoM3Vdi__DQnIIvkRohWPFvVDVnl3LP0AAAAATT_0QMA", default=None)
+FORCESUB = config("hackers_assemble", default=None)
+AUTH = config("737870357", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
